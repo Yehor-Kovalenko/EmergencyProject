71f102144e17603b341a917e416dc5b053b26042
szachnat <146722325+szachnat@users.noreply.github.com>
2024-12-05 17:16:31 +0100

    service, controller, entity

--- src/main/java/pl/io/emergency/service/Resource.java
+++ src/main/java/pl/io/emergency/service/Resource.java
@@ -2,22 +2,110 @@ package pl.io.emergency.service;
 
 import java.time.LocalDate;
 
+import jakarta.annotation.Nullable;
 import org.springframework.stereotype.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class Resource {
-    private int id;
+    private String id;
     private String type;
     private String description;
     private double amount;
     private LocalDate date;
     private String status;
-    private int destinationId;
-    private int holderId;
 
-    public boolean registerToDestination (String type, String description, double amount, int destinationId, int holderId)
+    private String destinationId;
+    private String holderId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDestinationId() {
+        return destinationId;
+    }
+
+    public void setDestinationId(String destinationId) {
+        this.destinationId = destinationId;
+    }
+
+    public String getHolderId() {
+        return holderId;
+    }
+
+    public void setHolderId(String holderId) {
+        this.holderId = holderId;
+    }
+
+    public Resource()
+    {}
+
+    public Resource (String type, String description, double amount, String destinationId, String holderId)
     {
-        return true;
+        this.type = type;
+        this.description = description;
+        this.amount = amount;
+        this.date =LocalDate.now();
+        this.status = "0";
+        this.destinationId = destinationId;
+        this.holderId = holderId; //user_id
+    }
+
+    public Resource (String type, String description, double amount, String holderId)
+    {
+        this.type = type;
+        this.description = description;
+        this.amount = amount;
+        this.date =LocalDate.now();
+        this.status = "0";
+        this.destinationId = null;
+        this.holderId = holderId; //przekazywany pzrze uzytlownika
     }
 
 }
--- src/test/java/pl/io/emergency/service/ResourceTest.java
+++ src/test/java/pl/io/emergency/service/ResourceTest.java
@@ -1,13 +1,54 @@
 package pl.io.emergency.service;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+
+//@SpringBootTest
+//@ContextConfiguration(classes = {Resource.class, S3ClientConfig.class})
 public class ResourceTest {
 
     @Test
-    public void first() {
-        Resource r = new Resource();
-        assertTrue(r.registerToDestination("type", "description", 20,1, 1));
+    public void testResourceConstructor1() {
+        String type = "medical";
+        String description = "First aid kits";
+        double amount = 50.0;
+        String destinationId = "1";
+        String holderId = "2";
+
+        // Utworzenie instancji klasy Resource z dynamicznymi danymi
+        Resource resource = new Resource(type, description, amount, destinationId, holderId);
+
+        // Testowanie czy wartości zostały poprawnie ustawione
+        assertEquals("medical", resource.getType());
+        assertEquals("First aid kits", resource.getDescription());
+        assertEquals(50.0, resource.getAmount());
+        assertEquals(LocalDate.now(),resource.getDate());
+        assertEquals("0",resource.getStatus());
+        assertEquals("1", resource.getDestinationId());
+        assertEquals("2", resource.getHolderId());
+    }
+
+    @Test
+    public void testResourceConstructor2() {
+        String type = "medical";
+        String description = "First aid kits";
+        double amount = 50.0;
+        String holderId = "2";
+
+        // Utworzenie instancji klasy Resource z dynamicznymi danymi
+        Resource resource = new Resource(type, description, amount, holderId);
+
+        // Testowanie czy wartości zostały poprawnie ustawione
+        assertEquals("medical", resource.getType());
+        assertEquals("First aid kits", resource.getDescription());
+        assertEquals(LocalDate.now(),resource.getDate());
+        assertEquals("0",resource.getStatus());
+        assertEquals(50.0, resource.getAmount());
+        assertEquals(null, resource.getDestinationId());
+        assertEquals("2", resource.getHolderId());
     }
 }
