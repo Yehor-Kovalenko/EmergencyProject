8800be6e2cccb8b5c99e3c67a9a5f513267004b3
freniecki <247773@edu.p.lodz.pl>
2024-12-06 12:51:38 +0100

    users definition

---
+++ src/main/java/pl/io/emergency/entity/User.java
@@ -0,0 +1,27 @@
+package pl.io.emergency.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
