5b76b125050ed18a11fd92e1d7f3cd9972e76ca6
a-vishniavetski <a.vishniavetski@gmail.com>
2024-12-08 23:51:04 +0100

    feat: Government reports endpoint is working; active catastrophe and resources, and giver reports, return full lists of all respective entities in the database; ReportType is implemented;

---
+++ src/main/java/pl/io/emergency/entity/ReportType.java
@@ -0,0 +1,11 @@
+package pl.io.emergency.entity;
+
+public enum ReportType {
+    GIVER,
+    ACTIVE_RESOURCES,
+    ARCHIVED_RESOURCES,
+    ACTIVE_VOLUNTEERS,
+    ARCHIVED_VOLUNTEERS,
+    ACTIVE_CATASTROPHES,
+    ARCHIVED_CATASTROPHES
+}
--- src/main/java/pl/io/emergency/service/ReportService.java
+++ src/main/java/pl/io/emergency/service/ReportService.java
@@ -1,11 +1,15 @@
 package pl.io.emergency.service;
 
 import org.springframework.stereotype.Service;
+import pl.io.emergency.entity.Catastrophe;
 import pl.io.emergency.entity.Report;
+import pl.io.emergency.entity.ReportType;
 import pl.io.emergency.entity.ResourceEntity;
+import pl.io.emergency.repository.CatastropheRepository;
 import pl.io.emergency.repository.ReportRepository;
 import pl.io.emergency.repository.ResourceRepositorium;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -14,10 +18,14 @@ import java.util.Optional;
 public class ReportService {
     private final ReportRepository reportRepository;
     private final ResourceRepositorium resourceRepositorium;
+    private final CatastropheRepository catastropheRepositorium;
 
-    public ReportService(ReportRepository reportRepository, ResourceRepositorium resourceRepositorium) {
+    public ReportService(ReportRepository reportRepository,
+                         ResourceRepositorium resourceRepositorium,
+                         CatastropheRepository catastropheRepositorium) {
         this.reportRepository = reportRepository;
         this.resourceRepositorium = resourceRepositorium;
+        this.catastropheRepositorium = catastropheRepositorium;
     }
 
     public List<Report> findAll() {
@@ -41,5 +49,19 @@ public class ReportService {
          List<ResourceEntity> allResources = this.resourceRepositorium.findAll();
 
         return new Report(localDateTime, allResources);
+    }
+
+    public Report getGovernmentReport(ReportType reportType, LocalDate dateFrom, LocalDate dateTo) {
+        if (reportType == ReportType.ACTIVE_CATASTROPHES){
+            // get all active catastrophes
+            List<Catastrophe> allActiveCatastrophes = this.catastropheRepositorium.findAll();
+            return new Report(reportType, LocalDateTime.now(), dateFrom, dateTo, allActiveCatastrophes);
+        }
+        else if (reportType == ReportType.ACTIVE_RESOURCES){
+            // get all active resources
+            List<ResourceEntity> allActiveResources = this.resourceRepositorium.findAll();
+            return new Report(reportType, LocalDateTime.now(), dateFrom, dateTo, allActiveResources);
+        }
+        return new Report(reportType, LocalDateTime.now(), dateFrom, dateTo, null);
     }
 }
