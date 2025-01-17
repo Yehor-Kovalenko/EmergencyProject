8d0c8e94ecca87c9d2dd4b2f664681cd2c99b487
freniecki <247773@edu.p.lodz.pl>
2025-01-17 17:32:55 +0100

    minor adjustments

--- src/main/java/pl/io/emergency/repository/UserRepository.java
+++ src/main/java/pl/io/emergency/repository/UserRepository.java
@@ -2,7 +2,7 @@ package pl.io.emergency.repository;
 
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
-import pl.io.emergency.entity.User;
+import pl.io.emergency.entity.users.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
