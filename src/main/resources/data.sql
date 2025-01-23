9759d5b6ca969d1d8c2723b0b8b48ad542c360c4
freniecki <247773@edu.p.lodz.pl>
2025-01-23 17:44:37 +0100

    fixed ids

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,23 +1,23 @@
 -- db init for test usage 
-
+-- haslo do wszystkich userow --> 'password'
 INSERT INTO app_users (id, email, username, phone, password, role) VALUES
-    (1, 'giver@example.com', 'giver', '123-456-789', '$2a$10$3DEvp/rDvtdKXvSiJ7nwVOxOFZHOntXlk9C4fF6QrTJ4k5deUUaLK', 'GIVER');
+    (101, 'giver@example.com', 'giver', '123-456-789', '$2a$10$3DEvp/rDvtdKXvSiJ7nwVOxOFZHOntXlk9C4fF6QrTJ4k5deUUaLK', 'GIVER');
 INSERT INTO Giver (id, first_name, last_name, birth_date) VALUES
-    (1, 'John', 'Doe', '1990-01-01');
+    (101, 'John', 'Doe', '1990-01-01');
 
 INSERT INTO app_users (id, email, username, phone, password, role) VALUES
-    (2, 'ngo@example.com', 'ngo', '123-456-789', '$2a$10$3DEvp/rDvtdKXvSiJ7nwVOxOFZHOntXlk9C4fF6QrTJ4k5deUUaLK', 'NGO');
+    (102, 'ngo@example.com', 'ngo', '123-456-789', '$2a$10$3DEvp/rDvtdKXvSiJ7nwVOxOFZHOntXlk9C4fF6QrTJ4k5deUUaLK', 'NGO');
 INSERT INTO NGO (id, name, krs) VALUES
-    (2, 'Helping Hands', '1234567890');
+    (102, 'Helping Hands', '1234567890');
 
 INSERT INTO app_users (id, email, username, phone, password, role) VALUES
-    (3, 'official@example.com', 'official', '123-456-789', '$2a$10$3DEvp/rDvtdKXvSiJ7nwVOxOFZHOntXlk9C4fF6QrTJ4k5deUUaLK', 'OFFICIAL');
+    (103, 'official@example.com', 'official', '123-456-789', '$2a$10$3DEvp/rDvtdKXvSiJ7nwVOxOFZHOntXlk9C4fF6QrTJ4k5deUUaLK', 'OFFICIAL');
 INSERT INTO Official (id, official_name, regon) VALUES
-    (3, 'City Official', '0987654321');
+    (103, 'City Official', '0987654321');
 
 INSERT INTO app_users (id, email, username, phone, password, role) VALUES
-    (4, 'volunteer@example.com', 'volunteer', '123-456-789', '$2a$10$3DEvp/rDvtdKXvSiJ7nwVOxOFZHOntXlk9C4fF6QrTJ4k5deUUaLK', 'VOLUNTEER');
+    (104, 'volunteer@example.com', 'volunteer', '123-456-789', '$2a$10$3DEvp/rDvtdKXvSiJ7nwVOxOFZHOntXlk9C4fF6QrTJ4k5deUUaLK', 'VOLUNTEER');
 INSERT INTO Volunteer (id, ready_for_mark, birth_date, organization_id, available, first_name, last_name) VALUES
-    (4, true, '1995-05-05', 2, true, 'Jane', 'Smith');
+    (104, true, '1995-05-05', 102, true, 'Jane', 'Smith');
 
 SELECT * FROM app_users;
